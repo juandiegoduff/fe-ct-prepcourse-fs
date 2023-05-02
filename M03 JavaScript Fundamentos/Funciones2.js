a0fede42ebf959d4809d644586e89f23d0fe1085
juandiego <juandiegoduff@gmail.com>
2023-05-02 12:55:12 -0500

    Funciones parte 2

---
+++ M03 JavaScript Fundamentos/Funciones2.js
@@ -0,0 +1,14 @@
+//Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+  }
+
+function otraFuncion(){
+     return("El nombre retornado por la funcion "
+     +"'cuidadoConElConsoleLog' es:" + cuidadoConElConsoleLog('Juan Diego'));
+}
+function cuidadoconelreturn (nombre){
+    return (nombre);
+    console.log(nombre);
+}
