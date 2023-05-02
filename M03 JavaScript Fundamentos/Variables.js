13f0efada0f0b769498ca2ee3da4b8a23aecbf55
juandiego <juandiegoduff@gmail.com>
2023-05-02 10:29:53 -0500

    Mi primer codigo de variables

---
+++ M03 JavaScript Fundamentos/Variables.js
@@ -0,0 +1,25 @@
+// Variables String
+var bootcamp= 'Datos Strings';
+console.log(bootcamp);
+
+// Variables Numeros
+var numeroEntero = 12;
+console.log(numeroEntero);
+var numeroNegativo = -24;
+console.log(numeroNegativo);
+var numerDecimal = 11.75;
+console.log(numerDecimal);
+
+//Variables Booleanos
+var estoyEntendiendo = true;
+console.log(estoyEntendiendo);
+var estoyAburrido = false;
+console.log(estoyAburrido);
+
+//Variables Undefined
+var cajon1;
+console.log(cajon1);
+
+//Variables Nulos
+var Nada=null;
+console.log(Nada);
