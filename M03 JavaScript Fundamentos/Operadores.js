e7a077aa0d081bcd6b22645b36cb050e791cbcea
juandiego <juandiegoduff@gmail.com>
2023-05-02 10:44:19 -0500

    Operadores y Precedencias

---
+++ M03 JavaScript Fundamentos/Operadores.js
@@ -0,0 +1,28 @@
+// Operadores y precedencia
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Potenciacion
+console.log(2**5);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Adicion y strings
+console.log('HOLA '+'MUNDO');
+var nombre = 'JUAN DIEGO';
+var saludo = 'HOLA '+ nombre;
+console.log(saludo);
