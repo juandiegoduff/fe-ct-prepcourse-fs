ad006c86334050c7e0f6bcfab345255a9ed8a5d0
juandiego <juandiegoduff@gmail.com>
2023-05-03 10:55:58 -0500

    Ejemplos OperadoresLogicos

---
+++ M04 JavaScript Bucles/Operadoreslogicos.js
@@ -0,0 +1,34 @@
+//OPERADOR AND &&
+function mayorYMenorYPar(num) {
+	if( num > 5 && num < 10  && num  % 2 === 0 ) 
+    console.log(true);
+	else console.log(false);
+};
+mayorYMenorYPar(7);
+mayorYMenorYPar(8);
+
+// OPERADOR OR 
+function operadorOr(str) {
+    if (str === 'Henry' || str.length < 2) console.log(true);
+    else console.log(false);
+ }
+ operadorOr('Henry');
+ operadorOr('Javascript');
+ operadorOr('H');
+
+// NOT
+function negacion(permiso) {
+    if (permiso !== true) console.log('Tiene permiso');
+ }
+ negacion(true);
+ negacion(false);
+
+// CONDICIONES COMPLEJAS
+function condicionCompleja(num){
+    if(num > 9 && num % 2 === 0 || num === 3) console.log(true);
+    else console.log(false);
+}
+condicionCompleja(18);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(5);
