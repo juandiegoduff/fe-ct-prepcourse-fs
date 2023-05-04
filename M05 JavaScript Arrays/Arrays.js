3422dd609909d999c73526bd7aae5603de4780db
juandiego <juandiegoduff@gmail.com>
2023-05-03 22:06:29 -0500

    Arreglos o Arrays

---
+++ M05 JavaScript Arrays/Arrays.js
@@ -0,0 +1,18 @@
+// ARRAYS o ARREGLOS lista de compras
+var listaDeCompras = [];
+    listaDeCompras[3] = 'Tomates';
+    listaDeCompras[1] = 'Lechuga';
+console.log(listaDeCompras);
+
+// Ver lista de compras
+console.log(listaDeCompras[3]);
+
+// Ver lista de compras mediante una variable
+var elementoDelArray = listaDeCompras[3];
+console.log(elementoDelArray);
+
+// Metodo Lenght para ver la cantidad q tiene un arreglo o array
+console.log(listaDeCompras.length);
+
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa','Juan'].length;
+console.log(nombres);
