f3dece40bf059e8eb11467dd2700bfd7288d0a99
juandiegoduff <juandiegoduff@gmail.com>
2023-10-20 16:59:54 -0500

    Objeto.js

---
+++ M06 JavaScript Objetos/Objeto.js
@@ -0,0 +1,72 @@
+var deportes ={
+    conBalon:["Football","Basketball"],
+    sinBalon: ["Boxeo","Surf"],
+};
+var deportes ={
+    conBalon:["Football","Basketball"],
+    sinBalon: ["Boxeo","Surf"],
+};
+
+var persona ={nombre:"Lucas", edad:26, estudios: {esProgramador: true}};
+// console.log(persona.edad);
+// persona.nombre = 'Martín';
+// console.log(persona.nombre);
+var autos = {};
+autos.marcas = ["Ford", "Audi", "Ferrari"];
+// console.log(autos);
+delete autos.marcas;
+// console.log(autos);
+
+var misFunciones ={
+    saludar: function(){
+        console.log("Hola");
+    },
+};
+misFunciones.saludar();
+
+ // DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+// console.log(atuendos);
+
+var comidas = {};
+var diferenciaNotaciones = function (pUno, pDos){
+    comidas[pUno]=['Frutas', 'Vegetales'];
+    comidas[pDos]=['Hamburguesas', 'Papas Fritas'];
+};
+diferenciaNotaciones('saludable', 'noSaludable');
+console.log (comidas);
+var persona ={nombre:"Lucas", edad:26, estudios: {esProgramador: true}};
+// console.log(persona.edad);
+// persona.nombre = 'Martín';
+// console.log(persona.nombre);
+var autos = {};
+autos.marcas = ["Ford", "Audi", "Ferrari"];
+// console.log(autos);
+delete autos.marcas;
+// console.log(autos);
+
+var misFunciones ={
+    saludar: function(){
+        console.log("Hola");
+    },
+};
+misFunciones.saludar();
+
+ // DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+// console.log(atuendos);
+
+var comidas = {};
+var diferenciaNotaciones = function (pUno, pDos){
+    comidas[pUno]=['Frutas', 'Vegetales'];
+    comidas[pDos]=['Hamburguesas', 'Papas Fritas'];
+};
+diferenciaNotaciones('saludable', 'noSaludable');
+console.log (comidas);
+
